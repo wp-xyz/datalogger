d6ac8d8616a3f6889d3864d88c9fae0919086b8c
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-11-01 19:06:04 +0200

    fix make.sh

--- make.sh
+++ make.sh
@@ -56,7 +56,7 @@ function priv_main
         case ${1} in
             build)
                 priv_pkgsearch
-                priv_packages 'FPSpreadsheet' 'Synapse 40.1'
+                priv_packages 'FPSpreadsheet' 'Synapse40.1'
                 find 'source' -type 'f' -name '*.lpi' \
                     -exec lazbuild --no-write-project --recursive --no-write-project {} 1>&2 +
                 ;;
